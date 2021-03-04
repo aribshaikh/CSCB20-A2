fad65f08ca637b6f8f1a8d0c7d812f25f27baf84
owenbc44 <owenbalthazar@gmail.com>
2021-03-03 23:14:52 -0500

    replaced q1 with correct file

--- Question1.py
+++ Question1.py
@@ -2,19 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
 @app.route("/")
 def home():
-    return 'Hello World!'
+    return "Hello World"
 
 @app.route("/<dave>")
 def generateResponse(dave):
-    # if dave.isupper():
-    #     return("Welcome, " + dave.lower() + ", to my CSCB20 website")
-    # elif dave.islower():
-    #     return("Welcome, " + dave.upper() + ", to my CSCB20 website")
-    # else:
-    #     return "Go fuck yourself"
-    return dave
+    if dave.islower() and not hasNumbers(dave):
+        return("Welcome, " + dave.upper() + ", to my CSCB20 website") 
+    elif dave.isupper() and not hasNumbers(dave):
+        return("Welcome, " + dave.lower() + ", to my CSCB20 website") 
+    else:
+        result = ""
+        for i in dave:
+            if not i.isnumeric():
+                result += i
+    return ("Welcome, " + result + ", to my CSCB20 website") 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
