1df3c1cc2bc25f0c2ee3256835c5c098fed72ffb
owenbc44 <owenbalthazar@gmail.com>
2021-04-03 21:04:00 -0400

    git issues

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def retrieveGrades():
 
 @app.route('/remarkRequest', methods=['GET','POST'])
 def remarkRequest():
-	# if 'username' in session:
+	if 'username' in session:
 
 		student_session = session.get('student')
 		instructor_session = session.get('instructor')
